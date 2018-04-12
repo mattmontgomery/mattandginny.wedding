b04b91c03a74c34aa3aced12ba4c2e5d93269d18
Matt Montgomery <montgomery.matt@gmail.com>
2018-04-12 01:05:15 -0600

    Clean up mailchimp

--- src/index.js
+++ src/index.js
@@ -105,43 +105,22 @@ const Directions = () => {
 
 const EmailSignup = () => {
   return (
-    <div id="mc_embed_signup">
+    <div id="mc_embed_signup" className="signup">
+      <h4>{"Sign-up for email updates"}</h4>
       <form
         action="https://instagram.us17.list-manage.com/subscribe/post?u=0f053e3093d7bf7019324ee48&amp;id=2d4882412a"
         method="post"
-        id="mc-embedded-subscribe-form"
         name="mc-embedded-subscribe-form"
         className="validate"
         target="_blank"
         noValidate
       >
-        <div id="mc_embed_signup_scroll">
-          <div className="mc-field-group">
-            <label htmlFor="mce-EMAIL">Email Address </label>
-            <input
-              type="email"
-              defaultValue=""
-              name="EMAIL"
-              className="required email"
-              id="mce-EMAIL"
-            />
+        <div>
+          <div>
+            <label htmlFor="mce-EMAIL">{"Email Address"}</label>
+            <input type="email" defaultValue="" name="EMAIL" />
           </div>
-          <div id="mce-responses" className="clear">
-            <div
-              className="response"
-              id="mce-error-response"
-              style={{ display: "none" }}
-            />
-            <div
-              className="response"
-              id="mce-success-response"
-              style={{ display: "none" }}
-            />
-          </div>
-          <div
-            style={{ position: "absolute", left: "-5000px" }}
-            aria-hidden="true"
-          >
+          <div style={{ display: "none" }} aria-hidden="true">
             <input
               type="text"
               name="b_0f053e3093d7bf7019324ee48_2d4882412a"
@@ -149,14 +128,8 @@ const EmailSignup = () => {
               value=""
             />
           </div>
-          <div className="clear">
-            <input
-              type="submit"
-              value="Subscribe"
-              name="subscribe"
-              id="mc-embedded-subscribe"
-              className="button"
-            />
+          <div>
+            <input type="submit" value="Subscribe" name="subscribe" />
           </div>
         </div>
       </form>
